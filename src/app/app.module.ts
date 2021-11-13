3843d421d8fb60b6b8c3634fed81b482051e3907
Érico <erico.mirandaa@hotmail.com>
2021-11-13 17:53:29 -0300

    criando component footbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ListHotelsComponent } from './list-hotels/list-hotels.component';
 import { BeHostComponent } from './be-host/be-host.component';
 import { FormsModule } from '@angular/forms';
+import { FootbarComponent } from './footbar/footbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, NavbarComponent, ListHotelsComponent, BeHostComponent],
+  declarations: [AppComponent, HomePageComponent, NavbarComponent, ListHotelsComponent,
+  BeHostComponent, FootbarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
+  
 })
 export class AppModule {}
---
+++ src/app/footbar/footbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'airbnb-footbar',
+  templateUrl: './footbar.component.html',
+  styleUrls: ['./footbar.component.scss']
+})
+export class FootbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
